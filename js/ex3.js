3c498b25cc83b63cbb044d53ba4190259bc601d0
Robert Torres <robert2000torresg@gmail.com>
2023-03-09 12:16:47 -0800

    version 2

--- js/ex3.js
+++ js/ex3.js
@@ -4,9 +4,11 @@
 
 console.log("Torres Output from HW 6 Example 3");
 
+//finds all p elements
 const changeParagraphStyle = () => {
     const paragraphs = document.getElementsByTagName("p");
   
+    //for loop that bolds and highlights in yellow p elements
     for (let i = 0; i < paragraphs.length; i++) {
       const paragraph = paragraphs[i];
       paragraph.style.fontWeight = "bold";
--- js/ex4.js
+++ js/ex4.js
@@ -5,13 +5,16 @@
 console.log("Torres Output from HW 6 Example 4");
 
 const addCollegeLink = () => {
+    //creates link element in js
     const collegeLink = document.createElement("a");
     collegeLink.href = "https://www.csulb.edu/college-of-business";
     collegeLink.textContent = "College of Business";
   
+    //uses query selector to find is
     const isLink = document.querySelector("#is");
     const linksList = isLink.parentElement;
   
+    //inserts link before is
     linksList.insertBefore(collegeLink, isLink);
   };
   
